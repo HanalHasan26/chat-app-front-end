5b4619f411e71d1d74580943edd174ba84641c0b
muhammedadil01 <muhammedadil932@gmail.com>
2023-08-07 12:55:31 +0530

    start a chat app

---
+++ src/component/Signup.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Signup.css'
+
+function Signup() {
+  return (
+    <div className='container'>
+    <div className='left-side'>
+      <h1>Left side</h1>
+    </div>
+    <div className='right-side'>
+    <h1>Right side</h1>
+    </div>
+  </div>
+  )
+}
+
+export default Signup
